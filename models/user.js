f9b8b53eda32d24b3e32bafe6c1db5812bbadc3b
abracu <abracu@novation.com.co>
2018-01-28 17:08:33 -0500

    Creación del modelo useuario Se crea el modelo usuario con sus atributos,

---
+++ models/user.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    name: String,
+    sername: String,
+    nick: String,
+    email: String,
+    password: String
+    role: String,
+    image: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
