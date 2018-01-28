8991ae52dfcdc90a07b0c85f8efea3867e378ccf
abracu <abracu@novation.com.co>
2018-01-28 17:33:05 -0500

    Creación de otros Modelos Se crean los modelos de: Follow, Publication y Message

--- models/user.js
+++ models/user.js
@@ -4,13 +4,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = Schema({
-    name: String,
-    surname: String,
-    nick: String,
-    email: String,
-    password: String
-    role: String,
-    image: String
+        name: String,
+        surname: String,
+        nick: String,
+        email: String,
+        password: String
+        role: String,
+        image: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
