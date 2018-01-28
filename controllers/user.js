d9cee1fbd86a656e23985a56553db14e99bbfa94
abracu <abracu@novation.com.co>
2018-01-28 18:34:25 -0500

    Registro de usuarios en el Backend Se construye el metodo para guardar usuarios, acompañado de la funcionalidad para cifrar contraseñas con bcrypt.

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
+
 var User = require('../models/user');
 
 function home(req, res){
@@ -14,7 +16,42 @@ function pruebas(req, res){
     });
 }
 
+function saveUser(req, res){
+    var params = req.body;
+    var user = new User();
+
+    if(params.name && params.surname && params.nick && params.email && params.password){
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        // Guardar contraseña cifrada con bcrypt
+        bcrypt.hash(params.password, null, null, (err, hash) => {
+            user.password = hash;
+
+            user.save((err, userStored) => {
+                if(err) return res.status(500).send({ message: 'Error al guardar el usuario'});
+
+                if(userStored){
+                    res.status(200).send({user: userStored });
+                }else{
+                    res.status(404).send({message: 'No se ha registrado el usuario'});
+                }
+            })
+        })
+
+    }else{
+        res.status(200).send({
+            message: 'Envia todos los campos necesarios!!'
+        });
+    }
+}
+
 module.exports =  {
     home,
-    pruebas
+    pruebas,
+    saveUser
 }
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,6 @@ var api =  express.Router();
 
 api.get('/home', UserController.home);
 api.get('/pruebas', UserController.pruebas);
+api.post('/register', UserController.saveUser);
 
 module.exports = api;
