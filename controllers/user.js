c11ca9773c3a36ef3be742136163d132dc71b16d
abracu <abracu@novation.com.co>
2018-01-28 21:32:08 -0500

    Usuarios Paginados Se crea metodo para listar usuarios con funcionalidad de paginacion.

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var bcrypt = require('bcrypt-nodejs');
+var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
 var jwt = require('../services/jwt');
@@ -114,11 +115,35 @@ function getUser(req, res){
     })
 
 }
+// Devolver un listado de usuarios paginados
+function getUsers(req, res){
+    var identity_user_id = req.user.sub;
+
+    var page = 1;
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+
+        if(!users) return res.status(404).send({message: 'El usuario no hay usuarios disponibles'});
+
+        return res.status(200).send({
+            users,
+            total,
+            pages: Math.ceil(total/itemsPerPage)
+        });
+    })
+}
 
 module.exports =  {
     home,
     pruebas,
     saveUser,
     loginUser,
-    getUser
+    getUser,
+    getUsers
 }
